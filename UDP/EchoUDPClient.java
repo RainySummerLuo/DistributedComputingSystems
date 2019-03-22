11b1a956f8883d401b471d9df385bc40c3c181bb
Zichen Luo <iluo@163.com>
2019-03-22 09:10:06 +0800

    Format Codes

--- UDP/EchoUDPClient.java
+++ UDP/EchoUDPClient.java
@@ -3,28 +3,23 @@ import java.net.*;
 import java.util.*;
 
 public class EchoUDPClient {
- public static void main(String[] args) {
-  Scanner sc = new Scanner(System.in);
-  String line = null;
-  try{
-  DatagramSocket soc = new DatagramSocket(); 
-  
-   while((line = sc.nextLine()) != null){
-           DatagramPacket dp = new DatagramPacket(line.getBytes(),
-                        line.getBytes().length,
-                       InetAddress.getByName("localhost"), //Server address
-                        7777);                                    
-     
-    soc.send(dp);
-    if(line.equals("bye")) break;
-    byte[] buffer = new byte[6255];
-    DatagramPacket receivePacket = new DatagramPacket(buffer, buffer.length);
-    soc.receive(receivePacket);
-    String msg = new String(buffer, 0, receivePacket.getLength());
-    System.out.println("Server says : " + msg);
-   }
-  }catch(IOException ex){
-   System.out.println(ex.getMessage());
-  }
- }
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        String line = null;
+        try{
+            DatagramSocket soc = new DatagramSocket();
+            while((line = sc.nextLine()) != null){
+                DatagramPacket dp = new DatagramPacket(line.getBytes(), line.getBytes().length, InetAddress.getByName("localhost"), 7777);
+                soc.send(dp);
+                if(line.equals("bye")) break;
+                byte[] buffer = new byte[6255];
+                DatagramPacket receivePacket = new DatagramPacket(buffer, buffer.length);
+                soc.receive(receivePacket);
+                String msg = new String(buffer, 0, receivePacket.getLength());
+                System.out.println("Server says : " + msg);
+            }
+        } catch(IOException ex){
+            System.out.println(ex.getMessage());
+        }
+    }
 }
